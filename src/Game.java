f5cd9d7937562afd9a37bf104dbfa5bcadb56da8
Aditi Kattuparambil <112968168+KattuparambilAditi@users.noreply.github.com>
2023-08-31 17:54:52 -0700

    added in methods

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,35 @@
 package src;
-import javax.swing.JPanel;
-import java.awt.Graphics;
+
+import processing.core.PApplet;
+
 
 /*layout and user inputs  + window and window updates
 */
+public class Game extends PApplet {
+	int c = color(152,190,100);
+	
+	public Game() {
 
-public class Game extends JPanel{
-    public void paint(Graphics g) {
-    
-    }
+	}
+	
+	
+	public void settings() {
+		setSize(600,500);
+	}
+	
+	//sets up the window. Background set to mossy green?
+	public void setup() {
+		background(c);
+
+	}
+	
+	//draws the person and the house
+	
+	public void draw() {
+
+	
+	}
+	
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,11 @@
 package src;
-
-import java.awt.Container;
-
-import javax.swing.JFrame;
-
-import src.Obstacles.Farmer;
+import processing.core.PApplet;
 
 /* Driver Class: displays the game */
 public class Main {
     public static void main(String[] args){
-	    JFrame window = new JFrame("Recycle Rush");
-	    window.setBounds(300, 10, 900, 800); 
-	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    Game game = new Game();
-	   
-	     
-	    Container c = window.getContentPane();
-	    c.add(game);
-	    
-	    window.setResizable(false);
-	    window.setVisible(true);
+	    Game drawing = new Game();
+		PApplet.runSketch(new String[]{""}, drawing);
+		drawing.windowResizable(true);
      }
 }
